c8f3c1b36af801bcf1c362626c9496ade6fc3ed2
Rina Villaruz <142535552+rinavillaruz@users.noreply.github.com>
2025-06-26 22:38:03 +0200

    Cleanup main.tf

--- environments/development/main.tf
+++ environments/development/main.tf
@@ -7,22 +7,3 @@ locals {
     development = module.development
   }
 }
-
-# output "private_key_path_from_module" {
-#   value = lookup(local.modules, terraform.workspace, null).private_key_path
-# }
-
-# output "public_key_path_from_module" {
-#   value = lookup(local.modules, terraform.workspace, null).public_key
-# }
-
-# output "rina_name" {
-#   value = "rina villaruz"
-# }
-
-# output "network_lb_dns" {
-#   value = lookup(local.modules, terraform.workspace, null).network_lb_dns
-# }
-
-# ######
-
